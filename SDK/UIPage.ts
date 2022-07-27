b51c8b5eb487296c6efad22a0ff8033d464bf2df
Marcos Vinicius Pereira de Oliveira <marcos8154@gmail.com>
2022-07-27 11:01:42 -0300

    update version

--- SDK/UIPage.ts
+++ SDK/UIPage.ts
@@ -19,7 +19,7 @@ import { IAppStorageProvider } from "./IAppStorageProvider";
  */
 export abstract class UIPage
 {
-    public static readonly PRODUCT_VERSION: string = '0.8.1'
+    public static readonly PRODUCT_VERSION: string = '0.8.9'
     public static DISABLE_EXCEPTION_PAGE: boolean = false;
     protected mainShell: PageShell;
 
